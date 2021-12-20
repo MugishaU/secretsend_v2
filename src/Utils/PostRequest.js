da863f531766b980a6964ca6749bfd325c8a9a32
MugishaU <muwiragiye123@gmail.com>
2021-12-20 16:30:37 +0000

    call api & return response

--- src/Utils/PostRequest.js
+++ src/Utils/PostRequest.js
@@ -1,20 +1,20 @@
-export default function PostRequest(body) {
-	const region = "eu-west-1"
-	const domain = "https://p3h7zn74oj.execute-api.eu-west-2.amazonaws.com" // e.g. search-domain.region.es.amazonaws.com
+export default async function PostRequest(body) {
+	const url = "https://p3h7zn74oj.execute-api.eu-west-2.amazonaws.com/email"
 
-	// Create the HTTP request
 	const request = {
 		body: JSON.stringify(body),
 		headers: {
 			"Content-Type": "application/json",
-			host: domain,
 		},
-		hostname: domain,
 		method: "POST",
-		path: "/email",
 	}
 
-	// Send the request
+	const promise = await fetch(url, request)
+	const response = await promise.json()
 
-	return process.env.REACT_APP_AWS_REGION
+	if (response.errors.length > 0) {
+		return response
+	} else {
+		return response.message
+	}
 }
